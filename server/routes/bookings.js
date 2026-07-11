ada1a036cdf1d3787aa4c17072f712c4cfaf03f9
Prabhav Agarwal <prabhavagarwal987@gmail.com>
2026-07-11 13:10:42 +0530

    Add Razorpay payment integration and QR code ticket generation (backend)

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { bookEvent, confirmBooking, getMyBookings, cancelBooking, sendBookingOTP } = require('../controllers/bookingController');
+const { bookEvent, confirmBooking, getMyBookings, cancelBooking, sendBookingOTP, createOrder, verifyPayment } = require('../controllers/bookingController');
 const { protect, admin } = require('../middleware/auth');
 
 router.post('/send-otp', protect, sendBookingOTP);
 router.post('/', protect, bookEvent);
+router.post('/:id/create-order', protect, createOrder);
+router.post('/:id/verify-payment', protect, verifyPayment);
 router.put('/:id/confirm', protect, admin, confirmBooking);
 router.get('/my', protect, getMyBookings);
 router.delete('/:id', protect, cancelBooking);
